b7641d16db6a3323209e32799c34b3ad8aa1f663
Gustavo <santosguhenrique@gmail.com>
2023-12-13 11:42:33 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaa

--- js/home.js
+++ js/home.js
@@ -3,11 +3,41 @@
 const button = document.querySelector("button")
 const modal = document.querySelector("dialog")
 const buttonClose = document.querySelector("dialog button")
+const bebidaContainers = document.querySelectorAll(".container-bebida");
 
+
+function openModal() {
+    modal.showModal();
+}
+
+
+bebidaContainers.forEach((container) => {
+    container.onclick = openModal;
+});
+
+
+buttonClose.onclick = function () {
+    modal.close();
+}
 button.onclick = function () {
     modal.showModal()
 }
 
-buttonClose.onclick = function (){
+const pizzaImages = document.querySelectorAll(".foto-pizza img")
+const bebidaImages = document.querySelectorAll(".container-bebida img")
+
+function openModal() {
+    modal.showModal()
+}
+
+pizzaImages.forEach((img) => {
+    img.onclick = openModal
+})
+
+bebidaImages.forEach((img) => {
+    img.onclick = openModal
+})
+
+buttonClose.onclick = function () {
     modal.close()
 }
